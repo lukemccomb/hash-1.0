8d29a579939716fb4f7a23da9017d983941dfa90
Luke McComb <lucasmccomb@gmail.com>
2014-06-19 14:53:40 -0600

    printed the first genres the people listed

--- 02_first_genre.rb
+++ 02_first_genre.rb
@@ -1,3 +1,5 @@
 require_relative 'people'
 
 # What is the first genre that each person listed?
+
+puts "The first genres each person listed are #{PEOPLE["joe"][:preferences][:favorite_genres][0]} and #{PEOPLE["sue"][:preferences][:favorite_genres][0]}."
