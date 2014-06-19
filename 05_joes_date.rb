36c32536d8dc56569d86ef6c7da514f672fa8d79
Luke McComb <lucasmccomb@gmail.com>
2014-06-19 15:15:44 -0600

    Date of Joe's first correspondence

--- 05_joes_date.rb
+++ 05_joes_date.rb
@@ -1,3 +1,5 @@
 require_relative 'people'
 
 # What was the date of the first correspondence with joe?
+
+puts "The date of the first correspondence with Joe is #{PEOPLE["joe"][:correspondence][0][:date]}."
