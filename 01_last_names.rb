f54481a64f7049d9440f233b5aa8b4d335fc6858
Luke McComb <lucasmccomb@gmail.com>
2014-06-19 14:33:39 -0600

    Printed the last names of all the people

--- 01_last_names.rb
+++ 01_last_names.rb
@@ -1,3 +1,5 @@
 require_relative 'people'
 
 # What are the last names of all of the people?
+
+puts "The last names of all the people are #{PEOPLE["joe"][:last_name]} and #{PEOPLE["sue"][:last_name]}."
