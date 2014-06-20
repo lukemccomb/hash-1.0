fd603e49643a164ac22f20b756a9ee5f8e9942c9
Luke McComb <lucasmccomb@gmail.com>
2014-06-20 10:47:43 -0600

    Printed the description of Sue's last correspondence

--- 06_sues_date.rb
+++ 06_sues_date.rb
@@ -1,3 +1,5 @@
 require_relative 'people'
 
 # What was the description of the last correspondence with sue?
+
+puts "The description of the last correspondence with sue is '#{PEOPLE["sue"][:correspondence][1][:description]}'."
